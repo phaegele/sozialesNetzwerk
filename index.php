fce7fdf7b24e680cadfdb7d4358295ddc3d355aa
Paul Haegele <p-haegele@gmx.de>
2020-11-12 18:43:27 +0100

    Version1

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+/**
+* Festlegung der Untergrenze der PHP-Version
+* @version 1.0
+*/
+if(0>version_compare(PHP_VERSION, '5')){
+    die ('<h1>Für diese Anwendung ist mindestens PHP 5 notwendig</h1>');
+}
+?>
+<!DOCTYPE html>
+
+<html lang="de">
+<head>
+	<meta charset="utf-8">
+	<title>Image2Food - Sag mir was ich daraus kochen kann - Index</title>
+	<meta name="viewport" content="width=device-width; initial-scale=1.0">
+</head>
+<body>
+	<div id="nav"></div>
+	<div id="content"></div>
+	<h1>Image2Food - Sag mir was ich daraus kochen kann</h1>
+	<h2>Das soziale, multimediale Netzwerk für Kochideen</h2>
+	<?php 
+	/** 
+	 * Das soziale Netzwerk f�r Kochideen
+	 * Die Einstiegsseite mit der Hauptklasse
+	 */
+	class index{
+		//
+	}
+	?>
+</body>
+</html>
