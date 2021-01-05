8f18eac31058407de91bcde3d3f6c9d702424a4d
ph <p-haegele@gmx.de>
2021-01-05 10:07:50 +0100

    korr_uploadform

--- uploadformular.inc.php
+++ uploadformular.inc.php
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-<h1 class="formueberschrift">Wählen Sie eine Datei zum Upload aus</h1>
-<form action="bildspeichern.php" method="post" enctype="multipart/form-data">  
-    <input name="datei" type="file" /><br/>
-    <h4 class="spezielleUeber">Zusätzliche Informationen zum Bild</h4>
-    <textarea name="zusatzinfos" rows="5" cols="110">
-    </textarea><br/>
-    <input type="submit" value="Starte upload" class="hlink" />
-</form>
-=======
 <h1 class="formueberschrift">Wählen Sie eine Datei zum Upload aus</h1>
 <form action="bildspeichern.php" method="post" enctype="multipart/form-data" id="uploadform">  
     <label class=reg_label>Bildauswahl</label>
@@ -20,4 +10,3 @@
     <input type="submit" value="Starte upload" class="hlink" />
     <script type="text/javascript" src="lib/js/index.js"></script>
 </form>
->>>>>>> 86e974af52d43cb4f66a1948df503d76dbba9e2f
